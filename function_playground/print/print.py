5554550b7a8449380ac1beb1ce8c0ef5ff72224d
Zhichs <zizic.kovacevic.tonci@gmail.com>
2023-08-06 20:13:24 +0200

    Playing around with print()

---
+++ function_playground/print/print.py
@@ -0,0 +1,19 @@
+sentence = ("I hope you are having a great day learning Python")
+
+print(len(sentence))
+
+
+multi_line_string = """Holly molly guackamole
+Bila je velika vrucina
+Sparina je bila
+Nije se moglo igrat""".title().swapcase()
+
+print(multi_line_string)
+
+
+first_name = "tonci"
+last_name = "zizaa"
+space = " "
+full_name = (first_name + space + last_name).upper()
+
+print(full_name)
