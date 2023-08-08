d5fe0639a7d69dce6ba2e93d0089038b734a4b65
Zhichs <zizic.kovacevic.tonci@gmail.com>
2023-08-08 22:22:30 +0200

    Daily studiying

---
+++ conditionals_playground/if_elif.py
@@ -0,0 +1,20 @@
+def main():
+    
+    # * Calling the funciton
+    compare()
+
+def compare():
+    
+    # Getting the input
+    x = int(input("What is X? "))
+    y = int(input("What is Y? "))
+    
+    # * Evaluating the numbers
+    if x != y:
+        print("X is not equal to Y")
+    else:
+        print("x is equal to y")
+
+
+main()
+
---
+++ conditionals_playground/if_elif2.py
@@ -0,0 +1,23 @@
+def main():
+    """
+    a = input("Choose an action:\n1.Play\n2.Load Game\n3.Settings\n4.Quit\nYour choice: ")
+
+    if a in ["1", "2", "3", "4"]:
+        if a == "1":
+            print("Starting the game...")
+        elif a == "2":
+            print("Loading your saved game...")
+        elif a == "3":
+            print("Opening settings...")
+        elif a == "4":
+            print("Exiting the game. Goodbye!")
+    else:
+        print("Error!")
+    """
+    
+    
+for i in range(10000000000):
+    if i % 43134122 == 0:
+        print(f"{i} is divisible by 4313412")
+
+main()
